9cc672f6956dbd2abdf5da6c3b79d524bed74304
BirhanuGardie <106751124+BirhanuGardie@users.noreply.github.com>
2024-11-18 11:50:26 +0300

    Update and row

--- Netflix-clone/src/utils/requests.jsx
+++ Netflix-clone/src/utils/requests.jsx
@@ -1,13 +1,14 @@
-const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = "119cdeca37a1f559bc8267592c322be7";
 const requests = {
-  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  fetchTopRatedMovies: `/movie/toprated?api_key=${API_KEY}&language=en-US`,
-  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
-  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
-  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
-  fetchRomanticMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
-  fetchRomanticMovies: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
-  fetchRomanticMovies: `/tv/popular?api_key=${API_KEY}&language=en-US&page=1`,
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+    fetchTvShow: `tv/popular?api_key=${API_KEY}&language=en-US&page=1`
 };
+
 export default requests;
